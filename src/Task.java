ea8bcf62d1cbbaf751453280475e3825e07b24d7
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-16 00:48:58 +0300

    Change2

--- src/Task.java
+++ src/Task.java
@@ -3,30 +3,30 @@ import java.util.Arrays;
 public class Task {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int start = 4;
+        int start = 61;
         int end = 9;
         int[] result = getSubArrayBetween(numbers, start, end);
         System.out.println(Arrays.toString(result));
         System.out.print("[");
-        for (int x = 0; x < result.length - 1; x++) {
-            System.out.print(result[x] + ", ");
+        for (int i = 0; i < result.length - 1; i++) {
+            System.out.print(result[i] + ", ");
         }
         System.out.print(result[result.length - 1] + "]");
     }
 
     public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
         int lengthOfNewMassive = 0;
-        for (int x = 0; x < numbers.length; x++) {
-            if (numbers[x] >= start && numbers[x] <= end) {
-                lengthOfNewMassive = lengthOfNewMassive + 1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= start && numbers[i] <= end) {
+                lengthOfNewMassive ++;
             }
         }
         int[] result = new int[lengthOfNewMassive];
         int counter = 0;
-        for (int x = 0; x < numbers.length; x++) {
-            if (numbers[x] >= start && numbers[x] <= end) {
-                result[counter] = numbers[x];
-                counter = counter + 1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] >= start && numbers[i] <= end) {
+                result[counter] = numbers[i];
+                counter ++;
             }
         }
         return result;
