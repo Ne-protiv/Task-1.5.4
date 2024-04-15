22f6e32fae4c8c4c03be52a845473a61db58b74a
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-15 18:09:06 +0300

    result

---
+++ src/Task.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Task {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int start = 4;
+        int end = 9;
+        int[] result = getSubArrayBetween(numbers, start, end);
+        System.out.println(Arrays.toString(result));
+        System.out.print("[");
+        for (int x = 0; x < result.length - 1; x++) {
+            System.out.print(result[x] + ", ");
+        }
+        System.out.print(result[result.length - 1] + "]");
+    }
+
+    public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
+        int lengthOfNewMassive = 0;
+        for (int x = 0; x < numbers.length; x++) {
+            if (numbers[x] >= start && numbers[x] <= end) {
+                lengthOfNewMassive = lengthOfNewMassive + 1;
+            }
+        }
+        int[] result = new int[lengthOfNewMassive];
+        int Counter = 0;
+        for (int x = 0; x < numbers.length; x++) {
+            if (numbers[x] >= start && numbers[x] <= end) {
+                result[Counter] = numbers[x];
+                Counter = Counter + 1;
+            }
+        }
+        return result;
+    }
+}
